ccf5ee206e61f49a006b5c0633e55c31c9040bdf
Gonzalo Giambagno <giambagno.gonzalo@gmail.com>
2022-05-21 20:23:01 -0300

    navBar

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,23 @@
 import './App.css';
+import theme from './temaConfig';
+import { ThemeProvider } from '@mui/material/styles';
+// import { makeStyles } from '@mui/material/styles';
+// import { Button } from '@mui/material/'
+// import Typography from '@mui/material/Typography'
 
 function App() {
   return (
-    <div >
-      
-    </div>
+    <ThemeProvider theme={theme}>
+      <header>
+
+
+
+
+
+
+        
+      </header>
+    </ThemeProvider>
   );
 }
 
