e10db13fecf14ff43e608bf00293e794516eb688
Fran <fran.texido@gmail.com>
2022-05-23 19:12:17 -0300

    arrancando cines4D

---
+++ my-app/src/components/cines4D.js
@@ -0,0 +1,13 @@
+import {Paper,Container } from '@mui/material/';
+
+
+export default function Cines4d() {
+    return (
+      <Container maxWidth="sm">
+      <Paper elevation={2} square>
+        El cine 4D es un sistema de proyección de películas que recrea en la sala de proyección las condiciones físicas que se ven en la pantalla, como niebla, lluvia, viento, sonidos más intensos u olores, así como vibraciones en los asientos y otros efectos, con el objetivo de generar una mayor inmersión del público con respecto a una sala tradicional.Las salas de cine en 4D pueden proyectar tanto en formato 3D como en formato 2D 
+      </Paper>
+      </Container>
+
+  );
+  }
