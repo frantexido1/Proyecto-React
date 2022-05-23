5896e21f262f8e0677db26979361420e8cf96f0d
Fran <fran.texido@gmail.com>
2022-05-23 20:39:01 -0300

    Cines avance

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
+import Cines4d from './components/cines4D';
+import {Container,Grid} from '@mui/material';
 
 function App() {
   return (
-    <div >
-      
-    </div>
+    <>
+    <p>sdasdajsdajsdjasdhashdas</p><Container maxWidth="md">
+      <Grid container spacing={3} sx={{ padding: "15px", alignContent: "center", backgroundColor: "#251F1F", borderRadius: "10px", boxShadow: "10px" }}>
+        <Cines4d />
+      </Grid>
+
+
+    </Container>
+    </>
+
+    
   );
 }
 
--- my-app/src/components/cines4D.js
+++ my-app/src/components/cines4D.js
@@ -1,13 +1,21 @@
-import {Paper,Container } from '@mui/material/';
+import {Paper,Grid} from '@mui/material/';
+
 
 
 export default function Cines4d() {
     return (
-      <Container maxWidth="sm">
-      <Paper elevation={2} square>
-        El cine 4D es un sistema de proyección de películas que recrea en la sala de proyección las condiciones físicas que se ven en la pantalla, como niebla, lluvia, viento, sonidos más intensos u olores, así como vibraciones en los asientos y otros efectos, con el objetivo de generar una mayor inmersión del público con respecto a una sala tradicional.Las salas de cine en 4D pueden proyectar tanto en formato 3D como en formato 2D 
-      </Paper>
-      </Container>
+      <><Grid item xs={6} >
+        <Paper elevation={3} square sx={{backgroundColor:"rgba(94, 94, 94, 0.5)"}}>
+          <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/6/68/Diagrama_de_teatro_4D.jpg/330px-Diagrama_de_teatro_4D.jpg' alt=''/>
+        </Paper>
+        </Grid>
+        <Grid item xs={6}>
+          <Paper elevation={3} square sx={{backgroundColor:"rgba(94, 94, 94, 0.5)"}}>
+            <h2>CINES 4D</h2>El cine 4D es un sistema de proyección de películas que recrea en la sala de proyección las condiciones físicas que se ven en la pantalla, como niebla, lluvia, viento, sonidos más intensos u olores, así como vibraciones en los asientos y otros efectos, con el objetivo de generar una mayor inmersión del público con respecto a una sala tradicional.Las salas de cine en 4D pueden proyectar tanto en formato 3D como en formato 2D
+          </Paper>
+        </Grid>
+      </>
+
 
   );
   }
