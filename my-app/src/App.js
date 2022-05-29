b3a873279f7671904b519d756f22f1b7cbef1838
Ricar91 <ricardo.ariel.rinaldi@gmail.com>
2022-05-29 14:19:50 -0300

    cambios cards

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,15 +2,37 @@ import React from 'react';
 import { ThemeProvider } from '@mui/material';
 import { Navbar } from './Components/Navbar';
 import theme from './configuracionMUI/theme';
-import { Carrusel } from "./Components/Carrusel";
+// import Cards from './Components/Cards';
+import {Carrusel} from './Components/Carrusel';
+import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid';
+import Peliculas from "./Components/db.json"
+
 
 
 function App() {
   return (
+     <div classname="App">
     <ThemeProvider theme={theme}>
       <Navbar/>
-      <Carrusel/>
-    </ThemeProvider>
+    </ThemeProvider> 
+
+
+    <Container sx={{ marginY: 5}}>
+
+
+    {Peliculas.map((pelicula) => (
+      <>
+       <Grid >
+       { pelicula.funciones.map((funcion, index) => (
+        <Carrusel funcion={funcion} key={index}/>
+       ))}
+       </Grid>
+  
+     </>
+     ))};
+    </Container>
+    </div>
   );
 }
 
--- my-app/src/Components/Carrusel.js
+++ my-app/src/Components/Carrusel.js
@@ -3,17 +3,54 @@
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
-import {datosCarrusel} from "./Data";
-import "./Carrusel.css";
+// import "./Carrusel.css";
+// import {datosCarrusel} from '../Components/Data.js';
+import "./Cards.css";
+import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import { Typography } from "@mui/material";
+import IconButton from '@mui/material/IconButton';
+import ConfirmationNumberIcon from '@mui/icons-material/ConfirmationNumber';
+import Stack from '@mui/material/Stack';
+import {  createTheme, ThemeProvider } from '@mui/material';
 
-export const Carrusel = () => {
-    const settings = {
-        dots: true,
-        infinite: false,
-        speed: 500,
-        slidesToShow: 4,
-        slidesToScroll: 4,
-        initialSlide: 0,
+
+const theme = createTheme({
+  components: {
+    MuiTypography: {
+      variants: [
+        {
+         props: {
+          variant: "h6"
+        },
+        style: { 
+          
+        },
+       },
+       {
+        props: {
+          variant: "body1"
+         
+        },
+        style: { 
+          
+        },
+       },
+     ],
+    },
+   },
+ });
+
+export const Carrusel = ({funcion}) => {
+      const settings = {
+      dots: true,
+      infinite: false,
+      speed: 500,
+      slidesToShow: 4,
+      slidesToScroll: 4,
+      initialSlide: 0,
+
         responsive: [
           {
             breakpoint: 1024,
@@ -22,7 +59,7 @@ export const Carrusel = () => {
               slidesToScroll: 3,
               infinite: true,
               dots: true
-            }
+            },
           },
           {
             breakpoint: 600,
@@ -30,22 +67,63 @@ export const Carrusel = () => {
               slidesToShow: 2,
               slidesToScroll: 2,
               initialSlide: 2
-            }
+            },
           },
           {
             breakpoint: 480,
             settings: {
               slidesToShow: 1,
-              slidesToScroll: 1
-            }
-          }
-        ]
+              slidesToScroll: 1,
+            },
+          },
+        ],
       };
   
+
     return (
-    <div classname="Carrusel">
+      <div classname="carrusel">
         <Slider {...settings}>
-        {datosCarrusel.map((item)=>(
+
+          <div>
+          <Grid item xs={3}>
+
+              
+           <ThemeProvider theme={theme}>
+            <Paper elevation={3} square >
+            <img src={funcion.img} alt="Scream" className="img"/>
+            <Box paddingX={1}>
+
+            <Typography variant="h6" component="h2" textAlign={"center"} >
+             {funcion.title} 
+            </Typography>
+
+
+            <Stack alignItems="center" paddingY={1}>
+                  <IconButton aria-label="ConfirmationNumberIcon">
+                     <ConfirmationNumberIcon sx={{ color: '#251F1F' }} />
+                   </IconButton>  
+            </Stack>
+
+
+            <Typography variant="body1" component="h2" textAlign={"center"} paddingY={1} >
+            {funcion.category} 
+            </Typography>
+
+          </Box> 
+        </Paper>
+      </ThemeProvider>
+
+     </Grid>
+     </div>
+        </Slider>
+        </div>
+
+   );
+}
+
+
+
+        /* /* {datosCarrusel.map((item) => (
         <div classname="card">
           <div classname="card-top">
             <img src={item.linkImg} alt={item.title}/>
@@ -53,14 +131,7 @@ export const Carrusel = () => {
           </div>
           <div classname="card-bottom">
             <h3>{item.price}</h3>
-            <p className="category">{item.category}</p>
+            <span className="category">{item.category}</span>
           </div>
         </div>
-        ))}
-        </Slider>
-    </div>
-   );
-}
-
-
-export default Carrusel;
+        ))} */
