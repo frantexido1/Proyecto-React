e9ba732fae4ac47c8c26a48414eed2745138619f
Fran <fran.texido@gmail.com>
2022-06-08 21:16:06 -0300

    Avance Modal

--- my-app/src/Components/ButtonModal.js
+++ my-app/src/Components/ButtonModal.js
@@ -1,13 +1,14 @@
-import { Button,Modal,Box} from "@mui/material";
+import { Button,Modal,Box,useTheme, TextField} from "@mui/material";
 import { useState } from "react";
 import SelectModal from "./SelectModal";
+import SelectModal2 from "./SelectModal2";
 
 const style = {
     position: 'absolute',
     top: '50%',
     left: '50%',
     transform: 'translate(-50%, -50%)',
-    width: "700px",
+    width: "900px",
     bgcolor: 'background.paper',
     borderRadius: '10px',
     boxShadow: 24,
@@ -19,14 +20,15 @@ export default function ButtonModal(){
     const [open, setOpen] = useState(false);
     const handleOpen = () => setOpen(true);
     const handleClose = () => setOpen(false);
-    
+    const theme = useTheme();
 
-
+    const options=["Cinépolis Avellaneda","Cinépolis Merlo","Cinépolis Recoleta","Cinépolis Pilar","Cinépolis Lujan"];
+    const horarios=["10:00","11:00","12:00","14:00","15:00","15:00","16:00","17:00","18:00","19:00","20:00","21:00","22:00"];
 
     return (
 
           <div>
-            <Button onClick={handleOpen} color="secondary">Open modal</Button>
+            <Button onClick={handleOpen} color="secondary">Comprar Entradas</Button>
 
             <Modal
               open={open}
@@ -35,8 +37,10 @@ export default function ButtonModal(){
               aria-describedby="modal-modal-description"
             >
                <Box sx={style}>
-                <SelectModal/>
-
+                <SelectModal option={options} title="Seleccione Cine"/>
+                <SelectModal2/>
+                <SelectModal option={horarios} title="Seleccione Horario" tamaño={{width:"50%"}}/>
+                {/* <TextField inputProps={{ inputMode: 'numeric', pattern: '[0 - 9]*' }} fullWidth label="Ingrese la Cantidad de Entradas" /> */}
                 </Box> 
             </Modal>
 
--- my-app/src/Components/SelectModal.js
+++ my-app/src/Components/SelectModal.js
@@ -1,27 +1,23 @@
 import { InputLabel, FormControl,Select,MenuItem} from "@mui/material";
 import { useState } from "react";
 
-export default function SelectModal(prop){
+export default function SelectModal({option,title,tamaño}){
     const [cine,setCine]=useState("");
     const handleChange = (e) => {
       setCine(e.target.value);
     };
     return (
-        <FormControl fullWidth color="secondary">
-        <InputLabel id="demo-simple-select-label">Seleccione Cine</InputLabel>
+        <FormControl fullWidth sx={tamaño} color="secondary">
+        <InputLabel id="demo-simple-select-label">{title}</InputLabel>
           <Select 
           labelId="demo-simple-select-label" 
           id="demo-simple-select" 
           value={cine} 
           label="Seleccione Cine" 
           onChange={handleChange}
-          color="secondary"
+          sx={{marginBottom:"30px",color:"text.secondary"}}
           >
-            <MenuItem value={1}>Cinépolis Avellaneda</MenuItem>
-            <MenuItem value={2}>Cinépolis Merlo</MenuItem>
-            <MenuItem value={3}>Cinépolis Recoleta</MenuItem>
-            <MenuItem value={4}>Cinépolis Pilar</MenuItem>
-            <MenuItem value={5}>Cinépolis Lujan</MenuItem>
+            {option.map((item,idx) => <MenuItem value={idx} sx={{color:"text.secondary"}}>{item}</MenuItem> )}
           </Select>
        </FormControl>
     )
